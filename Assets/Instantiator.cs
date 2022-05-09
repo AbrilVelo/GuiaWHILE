981d51c3d2e6f9ee92e1bff0c9d2ae29988514fc
AbrilVelo <101335470+AbrilVelo@users.noreply.github.com>
2022-05-09 12:01:52 -0300

    clonarobjeto

---
+++ Assets/Instantiator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiator : MonoBehaviour
+{
+
+    public GameObject objectToClone;
+    public int cont;
+    public void CloneObject()
+    {
+       
+       while(cont > 0)
+        {
+            Instantiate(objectToClone);
+            cont--;
+        }
+        
+    }
+}
